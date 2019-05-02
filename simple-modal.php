47e2afe3aaa67e262bcba5e4a5444700ba5b84d2
Alexander Shibisty <shibisty.a.wezom@gmail.com>
2019-05-02 05:20:20 +0300

    hello world

---
+++ simple-modal.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Simple Modal
+    Plugin URI: https://github.com/alexander-shibisty
+    description: Не работает без WooCommerce. Используйте шорткод [simple_modal] для вставки
+    Version: 0.1
+    Author: Alexander Shibisty
+    Author URI: https://github.com/alexander-shibisty
+    License: MIT
+*/
